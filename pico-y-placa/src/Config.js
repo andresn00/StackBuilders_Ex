f495e6b62ba6cf36593bdd0fa9eb48cb864e72c3
andresn00 <andreslozada00@hotmail.com>
2021-06-19 11:19:56 -0500

    Config

--- pico-y-placa/src/Config.js
+++ pico-y-placa/src/Config.js
@@ -1,3 +1,23 @@
-const config = {
+import moment from 'moment'
 
+const format = 'hh:mm A'
+
+export const config = {
+    nonPermitedHours: [
+        {
+            start: moment('07:00 am', format),
+            end: moment('09:30 am', format)
+        },
+        {
+            start: moment('04:00 pm', format),
+            end: moment('07:30 pm', format)
+        }
+    ],
+    lastDigitsPerDay: {
+        monday: [1, 2],
+        tuesday: [3, 4],
+        wednesday: [5, 6],
+        thursday: [7, 8],
+        friday: [9, 0]
+    }
 }
