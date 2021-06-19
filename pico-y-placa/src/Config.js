624d3a3036c454d3d8c5c9f575bac444c3f48338
andresn00 <andreslozada00@hotmail.com>
2021-06-19 17:29:15 -0500

    Pico Y Placa check

--- pico-y-placa/src/Config.js
+++ pico-y-placa/src/Config.js
@@ -5,19 +5,19 @@ const format = 'hh:mm A'
 export const config = {
     nonPermitedHours: [
         {
-            start: moment('07:00 am', format),
-            end: moment('09:30 am', format)
+            start: '07:00 am',
+            end: '09:30 am'
         },
         {
-            start: moment('04:00 pm', format),
-            end: moment('07:30 pm', format)
+            start: '04:00 pm',
+            end: '07:30 pm'
         }
     ],
     lastDigitsPerDay: {
-        monday: [1, 2],
-        tuesday: [3, 4],
-        wednesday: [5, 6],
-        thursday: [7, 8],
-        friday: [9, 0]
+        Monday: [1, 2],
+        Tuesday: [3, 4],
+        Wednesday: [5, 6],
+        Thursday: [7, 8],
+        Friday: [9, 0]
     }
 }
--- pico-y-placa/src/utils.js
+++ pico-y-placa/src/utils.js
@@ -1,3 +1,29 @@
-const CarCanGoOut = (plateNumber, date, time) => {
-    
+import moment from 'moment'
+import config from './Config'
+
+export const CarCanGoOut = (plateNumber, date, time) => {
+    // date -> weekday
+    const weekDay = moment(date).format('dddd')
+    const weekDayArr = config.lastDigitsPerDay[weekDay]
+
+    // last digit of plate
+    const lastDigit = parseInt(plateNumber[plateNumber.length - 1])
+
+    // last digit indexOf() in weekday arr
+    if (weekDayArr?.indexOf(lastDigit) === -1) {
+        return true
+    }
+
+    // time isBetween() each hour
+    const nonPermitedHours = config.nonPermitedHours
+    const format = 'hh:mm A'
+    for (let i = 0; i < nonPermitedHours.length; i++) {
+        const startHour = moment(nonPermitedHours[i].start, format)
+        const endHour =  moment(nonPermitedHours[i].end, format)
+        const res = moment(time, format).isBetween(startHour, endHour, undefined, [])
+        if (res) {
+            return false
+        }
+    }
+    return true
 }
